c17be2ed7b4af69895afedad4ba847b1b2d47f12
Francesca Barletta <wallebar@gmail.com>
2024-05-23 17:01:16 +0200

    add query in pageController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
     public function index(){
+
+
+        $movies = Movie::all();
+        dd($movies);
         return view('welcome');
     }
 }
