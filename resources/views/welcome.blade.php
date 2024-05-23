4228ef43bbf070546b5054406c0adc63e0420bce
Francesca Barletta <wallebar@gmail.com>
2024-05-23 17:27:42 +0200

    add screen print of movie in cards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,24 @@
 @extends('layout.app')
 
-@section('title', 'home')
+@section('title', 'index')
 
 @section('content')
 
 <div class="container">
-    <h1>welcome</h1>
+    <h1>Movies</h1>
+    <div class="row justify-content-between">
+    @foreach($movies as $movie)
+
+        <div class="card m-2" style="width: 18rem;">
+                 <h3 class="card-text">{{ $movie['title'] }}</h3>
+                 <p class="card-text">Original-title: {{ $movie['original_title'] }}</p>
+                 <p class="card-text">Nationality: {{ $movie['nationality'] }}</p>
+                 <p class="card-text">Date: {{ $movie['date'] }}</p>
+                 <p class="card-text">Vote: {{ $movie['vote'] }}</p>
+ 
+        </div>
+@endforeach
+        </div>
 </div>
 
 @endsection
